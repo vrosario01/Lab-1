70da0e7371152691763b9bdd9f6d05203546e634
vrosario01 <vrosario01@saintmarys.edu>
2026-09-10 07:02:02 -0400

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,9 +1,5 @@
 def rectangle_area(length, width):
-  """ float, float -> None
-  function rectangle_area takes in parameters length and width
-  and prints the area of the rectangle
-  """
-  ### add your solution here
-
+  """function rectangle_area takes in parameters length and width
+  and prints the area of the rectangle """
   print(length * width)
 
